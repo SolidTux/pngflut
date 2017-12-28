f4aaf1ee90b06d31091e5fbab1fe56d28ec7837b
Daniel Hauck <mail@dhauck.eu>
2017-12-28 19:15:16 +0100

    Update for 34C3.

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,8 @@ use std::time::Duration;
 fn main() {
     let image = open("image.png").expect("image").to_rgba();
 
-    let x0 = 750;
-    let y0 = 800;
+    let x0 = 500;
+    let y0 = 0;
 
     let mut data = Vec::new();
 
@@ -18,12 +18,19 @@ fn main() {
         for y in 0..image.height() {
             let (r, g, b, a) = image.get_pixel(x, y).channels4();
             if a > 10 {
-                data.push(format!("PX {} {} {:02X}{:02X}{:02X}\n", x0+x, y0+y, r, g, b));
+                data.push(format!(
+                    "PX {} {} {:02X}{:02X}{:02X}\n",
+                    x0 + x,
+                    y0 + y,
+                    r,
+                    g,
+                    b
+                ));
             }
         }
     }
 
-    let nthreads = 1000;
+    let nthreads = 200;
 
     println!("Starting.");
 
@@ -31,24 +38,25 @@ fn main() {
         let mut handles = Vec::new();
         for n in 0..nthreads {
             let d = data.clone();
-            let indoff = n*d.len()/nthreads;
+            let indoff = n * d.len() / nthreads;
             handles.push(thread::spawn(move || {
                 loop {
-                    match TcpStream::connect("94.45.231.39:1234") {
+                    // match TcpStream::connect("94.45.231.39:1234") {
+                    match TcpStream::connect("151.217.47.77:8080") {
                         Ok(mut stream) => {
                             stream.set_nodelay(true).expect("set_nodelay call failed");
                             let mut i = indoff;
                             loop {
-                                i = (i+1) % d.len();
+                                i = (i + 1) % d.len();
                                 match stream.write_fmt(format_args!("{}", d[i])) {
-                                    Ok(_) => {},
+                                    Ok(_) => {}
                                     Err(_) => {
                                         println!("Write error, connecting again ..");
-                                        break
+                                        break;
                                     }
                                 }
                             }
-                        },
+                        }
                         Err(_) => {
                             println!("Connection error, trying again ...");
                             thread::sleep(Duration::from_secs(1))
